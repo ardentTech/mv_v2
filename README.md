a18f54768d0abdb4fd5b743067c8b413f40c3022
Jonathan D. Baker <jonathan@ardent.tech>
2025-02-17 15:15:24 -0700

    add hal and bsp deps and button led demo code

---
+++ README.md
@@ -0,0 +1,11 @@
+# MB V2
+A (hopefully) sane starting point for embedded Rust on the micro:bit v2.
+
+### Dev
+* Flash the board: `$ cargo embed`
+
+### TODO
+* configure and experiment with timestamps in defmt
+* unit tests
+* integration tests
+* host CLI to drive target hardware
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ use core::sync::atomic::{AtomicUsize, Ordering};
 use cortex_m as _;
 use cortex_m_rt::entry;
 use defmt_rtt as _;
-use defmt::{info, warn};
+use defmt::info;
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::{InputPin, OutputPin};
+use microbit::Board;
+use microbit::hal::Timer;
 use panic_halt as _;
 
 // WARNING may overflow and wrap-around in long-lived apps
@@ -15,5 +19,31 @@ defmt::timestamp!("{=usize}", COUNT.fetch_add(1, Ordering::Relaxed));
 #[entry]
 fn main() -> ! {
     info!("starting up...");
-    loop {}
+    let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+
+    // switch row1 on
+    board.display_pins.row1.set_high().unwrap();
+
+    let mut led1 = board.display_pins.col1;
+    let mut led2 = board.display_pins.col2;
+
+    loop {
+        if let Ok(true) = board.buttons.button_a.is_high() {
+            info!("button a is depressed");
+            let _ = led1.set_high();
+        } else {
+            info!("button a is pressed");
+            let _ = led1.set_low();
+        }
+
+        if let Ok(true) = board.buttons.button_b.is_high() {
+            info!("button b is depressed");
+            let _ = led2.set_high();
+        } else {
+            info!("button b is pressed");
+            let _ = led2.set_low();
+        }
+        timer.delay_ms(1000u32);
+    }
 }
